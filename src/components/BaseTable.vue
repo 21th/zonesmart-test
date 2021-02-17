9c65ab016052f43d8e83705f76798242234e5af3
Roman <sokolik321@yandex.ru>
2021-02-17 18:26:34 +0300

    stage commit

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    div
+        table
+            thead
+                tr
+                    td(v-for="(header, i) in headers" :key="i") {{ header.title }}
+            tbody
+                tr(v-for="item in items" :key="item.id")
+                    td(v-for="(column, i) in headers" :key="i")
+                        slot(
+                            :name="`columns.${column.value}`"
+                            :item="item[column.value]"
+                        ) {{ item[column.value] }}
+</template>
+
+<script>
+export default {
+    name: 'BaseTable',
+    props: {
+        items: {
+            type: Array,
+            default: () => ([]),
+        },
+        headers: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/OrderTable.vue
+++ src/components/OrderTable.vue
@@ -1,10 +1,38 @@
 <template lang="pug">
-div table
+div
+    base-table(:items="orderList" :headers="headers")
+        template(v-slot:columns.items="{ item }") {{ item.length }}
+        template(v-slot:columns.create_date="{ item }")
+            | {{ new Date(Date.parse(item)).toLocaleDateString() }}
 </template>
 
 <script>
+import BaseTable from '@/components/BaseTable.vue'
+
 export default {
     name: 'OrderTable',
+    components: { BaseTable },
+    props: {
+        orderList: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+    data() {
+        return {
+            headers: [
+                { title: 'ID', value: 'id' },
+                { title: 'Товары', value: 'items' },
+                { title: 'Дата заказа', value: 'create_date' },
+                { title: 'Статус', value: 'status' },
+                { title: 'Оплачено', value: 'is_paid' },
+                { title: 'Доставлено', value: 'is_shipped' },
+                { title: 'Покупатель', value: 'buyer' },
+                { title: 'Метод отправки', value: 'shipping_method' },
+                { title: 'Стоимость', value: 'total_price' },
+            ],
+        }
+    },
 }
 </script>
 
